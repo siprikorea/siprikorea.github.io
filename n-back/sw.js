3f6b69249e9739493b9fda974cccd6b565866ac6
Chanbong Park <chanbong@Gabes-MacBook-Air.local>
2026-06-26 22:15:44 +0900

    Rebuild n-back: code cleanup (no behaviour change)

--- n-back/sw.js
+++ n-back/sw.js
@@ -1,6 +1,6 @@
 // App-shell service worker for the Dual N-Back PWA. The cache name carries
 // the build version so each deploy is a new SW that supersedes the old one.
-const CACHE = 'dnb-5674b77bd28e';
+const CACHE = 'dnb-95eda33bb6ed';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html'])).catch(() => {}));
