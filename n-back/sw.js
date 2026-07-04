7973400901df67d2fcac3008dd5e0fbf0f3feb13
Chanbong Park <chanbong@Gabes-MacBook-Air.local>
2026-07-04 21:30:40 +0900

    Deploy web: modal scroll fix, uniform language pills, dynamic version

--- n-back/sw.js
+++ n-back/sw.js
@@ -1,6 +1,6 @@
 // App-shell service worker for the Dual N-Back PWA. The cache name carries
 // the build version so each deploy is a new SW that supersedes the old one.
-const CACHE = 'dnb-ae9057d30365';
+const CACHE = 'dnb-d39579a0bf49';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html'])).catch(() => {}));
