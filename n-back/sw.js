01c126305b398218e7c7973e5d1bcd179cf08f09
Chanbong Park <chanbong@Gabes-MacBook-Air.local>
2026-07-18 16:37:31 +0900

    Deploy web: keyboard controls

--- n-back/sw.js
+++ n-back/sw.js
@@ -1,6 +1,6 @@
 // App-shell service worker for the Dual N-Back PWA. The cache name carries
 // the build version so each deploy is a new SW that supersedes the old one.
-const CACHE = 'dnb-73c12ff4fb82';
+const CACHE = 'dnb-04e236630cbd';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html'])).catch(() => {}));
