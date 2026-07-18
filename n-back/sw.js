cab0babd52bc4a190724c8fbe5cb96d8b85528e4
Chanbong Park <chanbong@Gabes-MacBook-Air.local>
2026-07-18 16:23:33 +0900

    Deploy web: aggregated day/level history

--- n-back/sw.js
+++ n-back/sw.js
@@ -1,6 +1,6 @@
 // App-shell service worker for the Dual N-Back PWA. The cache name carries
 // the build version so each deploy is a new SW that supersedes the old one.
-const CACHE = 'dnb-f0c16360c3b1';
+const CACHE = 'dnb-3ebc7d8c65c9';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html'])).catch(() => {}));
